363b74d881fe109a2ade9cea6b0134571dc7ca11
Joshua Farmer <rhubarbjosh@outlook.com>
2025-05-12 11:22:26 +0100

    the

--- make.sh
+++ make.sh
@@ -1,8 +1,8 @@
 # Embedded C Compiler
-mkdir -p bin
-gcc src/main.c -o bin/ec -Os -Wall -Wextra -Wno-implicit-int
-./bin/ec -s example.c -o bin/output.s -m LINUX
-nasm bin/output.s -o bin/output.o -f elf32
-ld bin/output.o -o bin/output -m elf_i386
-./bin/output
-echo $?
+mkdir -p bin 
+gcc src/main.c -o bin/ec -Os -Wall -Wextra -Wno-implicit-int 
+./bin/ec -s example.c -o bin/output.s -m LINUX 
+nasm bin/output.s -o bin/output.o -f elf32 
+ld bin/output.o -o bin/output -m elf_i386 
+./bin/output 
+echo $? 
